a8b6813ee6f60e3cd67171aa194fd44070fb24ce
Paul Lockyer <plockyer@googlemail.com>
2025-03-28 09:07:24 +0000

    updated to use vites env logic

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,10 @@
-require('dotnet').config();
 
 /* 
     API Details
 */
-export const APIKEY = process.env.API_KEY;
-export const APIBASEURL = process.env.API_BASE_URL;
+const config = {
+    APIKEY: import.meta.env.VITE_API_KEY,
+    APIBASEURL: import.meta.env.VITE_API_BASE_URL,
+};
+
+export default config;
